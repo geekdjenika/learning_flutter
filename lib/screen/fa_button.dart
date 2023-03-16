5e5779f7583b708450b162c184bfeff3dd81c271
Geek DJENIKA <djenikaa@gmail.com>
2023-03-16 10:40:41 +0000

    Before cloud commit

--- lib/screen/fa_button.dart
+++ lib/screen/fa_button.dart
@@ -3,34 +3,19 @@ import 'package:flutter/material.dart';
 class FAButton extends StatelessWidget {
   const FAButton({Key? key}) : super(key: key);
 
+  //bool isClicked = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: Row(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          CircleAvatar(
-            maxRadius: MediaQuery.of(context).size.width * .08,
-            child: const Icon(
-              Icons.accessible
-            ),
-          )
-          /*Icon(
-            Icons.add_reaction,
-            //color: Colors.black,
-          ),
-          ToggleButtons(
-            selectedColor: Colors.teal,
-            selectedBorderColor: Colors.teal,
-            disabledColor: Colors.black,
-            disabledBorderColor: Colors.black,
-            borderWidth: .8,
-            isSelected: const [false,true],
-            mouseCursor: MouseCursor.defer,
-            children: const [Text("Submit"),Text("Cancel")],
-          ),*/
-        ],
+      body: Container(color: Colors.brown,),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //isClicked = !isClicked;
+        },
+        tooltip: 'Changer',
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        child: Icon(Icons.save_alt),
       ),
     );
   }
