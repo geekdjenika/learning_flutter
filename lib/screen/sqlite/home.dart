a978eec2ee42cb3e34f9dcee51b73686aa1c9a60
Geek DJENIKA <djenikaa@gmail.com>
2023-03-30 15:32:24 +0000

    Dependencies added and UI started!

--- lib/screen/sqlite/home.dart
+++ lib/screen/sqlite/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tools/services/sqlite/sql_helper.dart';
 
 class CRUDSQLite extends StatefulWidget {
   const CRUDSQLite({Key? key}) : super(key: key);
@@ -8,8 +9,44 @@ class CRUDSQLite extends StatefulWidget {
 }
 
 class _CRUDSQLiteState extends State<CRUDSQLite> {
+
+  List<Map<String, dynamic>> _journals = [];
+
+  void _refreshJurnals() async {
+    final data = await SQLHelper.getItems();
+    setState(() {
+      _journals = data;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _refreshJurnals();
+  }
+
+  void showForm(int? id) async {
+    if(id == null) {
+      showModalBottomSheet(context: context, builder: (_) => Card(
+
+      ));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+
+        },
+        child: Icon(
+          Icons.edit_note_rounded
+        ),
+      ),
+    );
   }
 }
